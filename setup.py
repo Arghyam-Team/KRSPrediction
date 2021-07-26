e569c6c40b84ea02d857940c5fd54145c33dfd62
abhinav <abhinav.dayal@hotmail.com>
2021-07-26 20:28:37 +0530

    title change

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ MODELS = [
     {'number': 5, 'title': '30 day Prediction from 30 day Window', 'folder':'90daysto30days', 'T':90, 'HORIZON': 30, 'reservoir': 'krs'},
     {'number': 6, 'title': '60 day Prediction from 60 day Window', 'folder':'60daysto60days', 'T':60, 'HORIZON': 60, 'reservoir': 'krs'},
     {'number': 7, 'title': '90 day Prediction from 90 day Window', 'folder':'90daysto90days', 'T':90, 'HORIZON': 90, 'reservoir': 'krs'},
-    {'number': 8, 'title': 'One day Prediction from 30 day Weather', 'folder':'30daysto1day_only_weather', 'T':30, 'HORIZON': 1, 'reservoir': 'krs', 'onlyweather':True}
+    {'number': 8, 'title': '90 day Prediction from 30 day Weather forecast', 'folder':'30daysto1day_only_weather', 'T':30, 'HORIZON': 1, 'reservoir': 'krs', 'onlyweather':True}
 ]
