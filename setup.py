682e071b70dc05e21e3bae3cc7d5706311f8f15f
abhinav <abhinav.dayal@hotmail.com>
2021-07-26 21:03:20 +0530

    title fix

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ MODELS = [
     {'number': 1, 'title': 'One day Prediction from 30 day Window', 'folder':'30daysto1day', 'T':30, 'HORIZON': 1, 'reservoir': 'krs'},
     {'number': 2, 'title': '15 day Prediction from 30 day Window', 'folder':'30daysto15days', 'T':30, 'HORIZON': 15, 'reservoir': 'krs'},
     {'number': 3, 'title': '30 day Prediction from 30 day Window', 'folder':'30daysto30days', 'T':30, 'HORIZON': 30, 'reservoir': 'krs'},
-    {'number': 4, 'title': '30 day Prediction from 30 day Window', 'folder':'60daysto30days', 'T':60, 'HORIZON': 30, 'reservoir': 'krs'},
-    {'number': 5, 'title': '30 day Prediction from 30 day Window', 'folder':'90daysto30days', 'T':90, 'HORIZON': 30, 'reservoir': 'krs'},
+    {'number': 4, 'title': '30 day Prediction from 60 day Window', 'folder':'60daysto30days', 'T':60, 'HORIZON': 30, 'reservoir': 'krs'},
+    {'number': 5, 'title': '30 day Prediction from 90 day Window', 'folder':'90daysto30days', 'T':90, 'HORIZON': 30, 'reservoir': 'krs'},
     {'number': 6, 'title': '60 day Prediction from 60 day Window', 'folder':'60daysto60days', 'T':60, 'HORIZON': 60, 'reservoir': 'krs'},
     {'number': 7, 'title': '90 day Prediction from 90 day Window', 'folder':'90daysto90days', 'T':90, 'HORIZON': 90, 'reservoir': 'krs'},
     {'number': 8, 'title': '90 day Prediction from 30 day Weather forecast', 'folder':'30daysto1day_only_weather', 'T':30, 'HORIZON': 1, 'reservoir': 'krs', 'onlyweather':True}
